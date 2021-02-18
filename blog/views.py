9b0b106f51e80b45301ea3007f5a14a698d57f09
Moronya <moronyasociety@gmail.com>
2021-02-18 23:57:24 +0300

    database added

--- blog/views.py
+++ blog/views.py
@@ -1,29 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
-
-posts = [
-    {
-        'author':'Amos Marwa',
-        'title':'Blog post 1',
-        'content':'First Post content',
-        'date_posted':'February 15, 2021'
-
-    },
-    {
-        'author':'Moronya Junior',
-        'title':'Blog post 2',
-        'content':'Second Post content',
-        'date_posted':'February 16, 2021'
-
-    }
-]
-
 def home(request):
     context = {
-        'posts':posts
-    }
+        'posts':Post.objects.all()
+            }
     return render(request, 'blog/home.html', context)
 def about(request):
     return render(request, 'blog/about.html', {'title':'About page'})
