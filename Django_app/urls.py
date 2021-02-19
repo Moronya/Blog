5fc6134cc02cbe1e1684ea8a012c3eb2641e8856
Moronya <moronyasociety@gmail.com>
2021-02-19 04:37:42 +0300

    user registration

--- Django_app/urls.py
+++ Django_app/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', user_views.register, name = 'register'),
     path('', include('blog.urls')), #the blog home is the homepage of our whole project
     
 ]
